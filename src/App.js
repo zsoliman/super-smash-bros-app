904b760f4e32345a2bc2b77281d537f191ce136b
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-20 11:47:01 -0400

    added flexbox and transition

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ function App() {
   const fighters = ['Roy', 'Ganondorf', 'Mario', 'Luigi', 'Marth', 'Link', 'Inkling', 'Samus', 'Fox', 'Sheik', 'Zelda', 'Ken']
 
   return (
+
     <div className="App">
       <h1>Fighters</h1>
       <div className='fighters-grid'>
 
+        {
+          fighters.map((element, index) => {
+            return (
+              <div>
+                <img src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${element.toLowerCase()}.png`} />
+              </div>
+            )
+          })
+        }
+
       </div>
     </div>
+
   );
 }
 
