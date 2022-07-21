63d4da75a91b64919beb2d0ac71a75601a1fc374
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-21 16:05:40 -0400

    set up css

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const FighterScreen = () => {
     return (
-        <div>
-            <h1>Fighter Screen</h1>
+        <div id="fighter-screen">
+            <img src="" className="char-model" />
+            <h3 className="char-name"></h3>
         </div>
     )
 }
