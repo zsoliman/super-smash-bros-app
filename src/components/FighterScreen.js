be1be6316013f54f5c534c20b52056262bcfb8b4
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-21 16:29:19 -0400

    added full image

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -3,7 +3,7 @@ import React from "react";
 const FighterScreen = ({ selectedFighter }) => {
     return (
         <div id="fighter-screen" style={{ backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/bg.jpg)`, backgroundSize: 'cover' }} >
-            <img src="" className="char-model" />
+            <img src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedFighter.toLowerCase()}/main.png`} className="char-model" />
             <h3 className="char-name">{selectedFighter}</h3>
         </div>
     )
